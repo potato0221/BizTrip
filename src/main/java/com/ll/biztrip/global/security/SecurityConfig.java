f21b61847fa9a053177fe69d99ffffd20d99d7f1
potato0221 <wnqo10@naver.com>
2025-05-01 20:45:49 +0900

    feat: ktx 도시 추가 front 연결

--- src/main/java/com/ll/biztrip/global/security/SecurityConfig.java
+++ src/main/java/com/ll/biztrip/global/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeRequests(authorizeRequests ->
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/gen/**")
                                 .permitAll()
